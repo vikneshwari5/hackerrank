1af2ad40167706619da7cd7722aae1e3411ccaf2
vikneshwari5 <vikneshwari.p945_mca27@mepcoeng.ac.in>
2026-03-05 19:14:09 +0530

    Create Java If-Else.java

---
+++ Java If-Else.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+
+        if (n % 2 != 0) {
+            // Case 1: n is odd
+            System.out.println("Weird");
+        } else {
+            // n is even
+            if (n >= 2 && n <= 5) {
+                System.out.println("Not Weird");
+            } else if (n >= 6 && n <= 20) {
+                System.out.println("Weird");
+            } else {
+                // n > 20
+                System.out.println("Not Weird");
+            }
+        }
+    }
+}
