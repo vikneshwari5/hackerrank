a6448e62189d59a23c284caf5a08b7d689185d06
vikneshwari5 <vikneshwari.p945_mca27@mepcoeng.ac.in>
2026-03-05 19:28:51 +0530

    Create Java Anagrams.java

---
+++ Java Anagrams.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        if (a == null || b == null || a.length() != b.length()) {
+            return false;
+        }
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        int[] frequencies = new int[256];
+
+        for (int i = 0; i < a.length(); i++) {
+            frequencies[a.charAt(i)]++;
+            frequencies[b.charAt(i)]--;
+        }
+
+        for (int count : frequencies) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
