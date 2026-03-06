01b8a5189bce321b22be4032f6117a22c89e83bf
vikneshwari5 <vikneshwari.p945_mca27@mepcoeng.ac.in>
2026-03-06 06:02:04 +0530

    Create Java Comparator.java

---
+++ Java Comparator.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+class Checker implements Comparator<Player> {
+    @Override
+    public int compare(Player a, Player b) {
+        if (a.score == b.score) {
+            return a.name.compareTo(b.name);
+        }
+        return b.score - a.score;
+    }
+}
+
